26e0c40b67816c730180e372abe1e80934a12c09
Job <jobtalle@hotmail.com>
2022-01-30 18:48:54 +0100

    Implemented M for mute

--- js/game/music.js
+++ js/game/music.js
@@ -5,11 +5,19 @@ export class Music {
         Sounds.MUSIC_0.play();
         Sounds.MUSIC_1.play();
         Sounds.MUSIC_2.play();
+
+        this.mute = 1;
+
+        window.addEventListener("keydown", event => {
+            if (event.key === "m") {
+                this.mute = 1 - this.mute;
+            }
+        })
     }
 
     setVelocity(velocity) {
-        Sounds.MUSIC_0.setVolume(Math.min(1, velocity / 20));
-        Sounds.MUSIC_1.setVolume(Math.sqrt(Math.max(0, Math.min(1, (velocity - 30) / 30))));
-        Sounds.MUSIC_2.setVolume(Math.sqrt(Math.max(0, Math.min(1, (velocity - 50) / 30))));
+        Sounds.MUSIC_0.setVolume(this.mute * Math.min(1, velocity / 20));
+        Sounds.MUSIC_1.setVolume(this.mute * Math.sqrt(Math.max(0, Math.min(1, (velocity - 30) / 30))));
+        Sounds.MUSIC_2.setVolume(this.mute * Math.sqrt(Math.max(0, Math.min(1, (velocity - 50) / 30))));
     }
 }
